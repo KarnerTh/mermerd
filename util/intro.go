2d6dec97387aa334e49f98eea5c15c8a3bdc05ff
Thomas Karner <thomas.7.karner@gmail.com>
2022-03-12 17:33:43 +0100

    show version in intro

--- util/intro.go
+++ util/intro.go
@@ -1,14 +1,18 @@
 package util
 
-import "github.com/fatih/color"
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"github.com/spf13/viper"
+)
 
 func ShowIntro() {
-	color.Green(`
+	color.Green(fmt.Sprintf(`
 .  ..___.__ .  ..___.__ .__ 
 |\/|[__ [__)|\/|[__ [__)|  \
-|  |[___|  \|  |[___|  \|__/
+|  |[___|  \|  |[___|  \|__/ (%s)
 
 Create MermaidJs diagrams from your database
 
-`)
+`, viper.Get("version")))
 }
