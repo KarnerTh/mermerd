bd1d2f3cf3dd4d3a37234936d5cf66149a746efd
Dan Goslen <dwgoslen@gmail.com>
2023-09-30 18:44:14 -0400

    Adds comments for label regex

--- config/relationship_label.go
+++ config/relationship_label.go
@@ -41,7 +41,15 @@ func parseLabel(label string) (RelationshipLabel, error) {
 	}, nil
 }
 
-var labelRegex = regexp.MustCompile(`([\w\._-]+)[\s]+([\w\._-]+)[\s]+:[\s]+([\w._-]+)`)
+// The regex works by creating three capture groups
+// Each group allows for all word characters, `.`, `_` and `-` any number of times
+// The first two groups (the table names) are separated by any amount of whitespace characters
+// The table names and label are are separated by
+// - any number of whitespace characters
+// - a `:`
+// - and then any other number of whitespace characters
+// The string must start with the first table name and it must end with the label
+var labelRegex = regexp.MustCompile(`^([\w\._-]+)[\s]+([\w\._-]+)[\s]+:[\s]+([\w._-]+)$`)
 
 func match(label string) (bool, [][]byte) {
 	groups := labelRegex.FindSubmatch([]byte(label))
