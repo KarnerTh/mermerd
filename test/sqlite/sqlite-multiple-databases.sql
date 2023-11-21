3c12151314eccd115a9a7fcba3379389a1dafadd
Thomas Karner <thomas.7.karner@gmail.com>
2023-11-21 19:55:41 +0100

    prepare sqlite database for ci

---
+++ test/sqlite/sqlite-multiple-databases.sql
@@ -0,0 +1,8 @@
+-- Test case for https://github.com/KarnerTh/mermerd/issues/23
+-- only stub, becuase sqlite does not support multiple databases
+create table test_3_a
+(
+    id    int          not null primary key,
+    title varchar(255) not null
+);
+
