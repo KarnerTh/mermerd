76c5d90dded0613b59168a772166eaee51718846
Thomas Karner <thomas.7.karner@gmail.com>
2024-12-29 12:51:34 +0100

    add goreleaser write permission; use latest 2019 mssql image

--- test/mssql/entrypoint.sh
+++ test/mssql/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Source: https://cardano.github.io/blog/2017/11/15/mssql-docker-container
-wait_time=30s
+wait_time=60s
 password=securePassword1!
 
 # wait for SQL Server to come up
@@ -9,9 +9,9 @@ sleep $wait_time
 echo importing data...
 
 # run the sql scripts to create the test database
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./mssql-setup.sql
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -d mermerd_test -i ./db-table-setup.sql
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -d mermerd_test -i ./mssql-enum-setup.sql
-/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -d mermerd_test -i ./mssql-multiple-databases.sql
+/opt/mssql-tools18/bin/sqlcmd -S 0.0.0.0 -C -U sa -P $password -i ./mssql-setup.sql
+/opt/mssql-tools18/bin/sqlcmd -S 0.0.0.0 -C -U sa -P $password -d mermerd_test -i ./db-table-setup.sql
+/opt/mssql-tools18/bin/sqlcmd -S 0.0.0.0 -C -U sa -P $password -d mermerd_test -i ./mssql-enum-setup.sql
+/opt/mssql-tools18/bin/sqlcmd -S 0.0.0.0 -C -U sa -P $password -d mermerd_test -i ./mssql-multiple-databases.sql
 
 echo importing done
---
+++ test/mssql/entrypoint_azure.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Source: https://cardano.github.io/blog/2017/11/15/mssql-docker-container
+wait_time=60s
+password=securePassword1!
+
+# wait for SQL Server to come up
+echo importing data will start in $wait_time...
+sleep $wait_time
+echo importing data...
+
+# run the sql scripts to create the test database
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -i ./mssql-setup.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -d mermerd_test -i ./db-table-setup.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -d mermerd_test -i ./mssql-enum-setup.sql
+/opt/mssql-tools/bin/sqlcmd -S 0.0.0.0 -U sa -P $password -d mermerd_test -i ./mssql-multiple-databases.sql
+
+echo importing done
